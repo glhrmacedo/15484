8f2b3f78d69be7a5477c0c20dea3a379bc325688
Guilherme Macedo <macedo@dca.fee.unicamp.br>
2026-08-24 12:23:18 -0300

    Create exemplo7.c

---
+++ exemplos/exemplo7.c
@@ -0,0 +1,43 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int adicao(int a, int b)
+{
+    return a + b;
+}
+
+int subtracao(int a, int b)
+{
+    return a - b;
+}
+
+int multiplicacao(int a, int b)
+{
+    return a * b;
+}
+
+int divisao(int a, int b)
+{
+    return a / b;
+}
+
+int resto(int a, int b)
+{
+    return a * b;
+}
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("a = %p\n", &a);
+    printf("a = %p\n", &b);
+    printf("a + b = %d\n", adicao(a, b));
+    printf("a - b = %d\n", subtracao(a, b));
+    printf("a * b = %d\n", multiplicacao(a, b));
+    printf("a / b = %d\n", divisao(a, b));
+    printf("a %% b = %d\n", resto(a, b));
+    return EXIT_SUCCESS;
+}
