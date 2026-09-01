91729d7b3e0b4423ade9bfe2ee76a9d2758ddee1
Guilherme Macedo <macedo@dca.fee.unicamp.br>
2026-09-01 15:05:02 -0300

    Create exemplo11.c

---
+++ exemplos/exemplo11.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a, b, c, delta;
+
+    printf("Escreva os coeficientes a, b e c da equação do segundo grau: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == 0)
+    {
+        printf("A equação não é do segundo grau, pois o coeficiente 'a' é zero.\n");
+    }
+    else
+    {
+        delta = b * b - 4 * a * c;
+
+        if (delta < 0)
+        {
+            printf("A equação do segundo grau não possui raízes reais.\n");
+        }
+        else
+        {
+            if (delta > 0)
+            {
+                printf("A equação do segundo grau possui duas raízes reais distintas.\n");
+            }
+            else
+            {
+                printf("A equação do segundo grau possui uma raiz real.\n");
+            }
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
